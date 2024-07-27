28dc101a37fbc093cc6b4d5e0b605702278323a7
Patricio <patocine@hotmail.cl>
2024-07-27 02:40:11 -0400

    add project

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+
+    <div className="App">
+      <header className="App-header">
+        <p>
+          AWS despliegue porjecto React!
+        </p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Arq Cloud
+        </p>
+  
+      </header>
+    </div>
+  );
+}
+
+export default App;
